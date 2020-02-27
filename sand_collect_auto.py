04a72adf1c7ea954b9799c3188e28ce194cf8bea
tomson784 <42789340+tomson784@users.noreply.github.com>
2020-02-27 14:26:00 +0900

    Update sand_collect_auto.py

--- sand_collect_auto.py
+++ sand_collect_auto.py
@@ -1,6 +1,6 @@
 # Qテーブルのみの学習
-# 通常移動、積み下ろし、積み上げ、壁の衝突のみで評価
-# 刻々と変化する地形は無視
+# 通常移動、積み下ろし、積み上げ、壁の衝突
+# 土砂の塊の重心から分散を求めて、分散が小さいほど報酬を上げる
 
 import pygame
 from pygame.locals import *
@@ -8,7 +8,7 @@ import sys
 import numpy as np
 import os
 from datetime import datetime
-SCR_RECT = Rect(0, 0, 640, 480) # 画面サイズ
+SCR_RECT = Rect(0, 0, 800, 640) # 画面サイズ
 
 # マップのクラス
 class Map:
@@ -35,10 +35,18 @@ def load_img(filename, colorkey=None):
     return img
 
 # 土砂の塊の重心の分散を計算して評価を変更する
-def eval_action():
-    pass
+def eval_action(x, y):
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    # eval_var = np.abs(np.mean((x - x_mean)*(y - y_mean)))
+    eval_var = np.mean((x - x_mean)**2) + np.mean((y - y_mean)**2)
+    # if eval_var == np.nan:
+    #     eval_var = 0.01
+    #     print(eval_var)
+    sys.stdout.write("      {:.5f}".format(eval_var))
+    return np.round(1/(eval_var+1), decimals=5)
 
-def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
+def main(max_step, episode, map_n, greed_e, r1, r2, r3, r4, r5, def_r, test, fps):
     save_path = "./log/{0}_map{1}".format(datetime.now().strftime("%y%m%d%H"), map_n)
     if test != True:
         os.makedirs(save_path)
@@ -77,7 +85,10 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
         goal_id = np.where(map.map==2)
         FPSCLOCK = pygame.time.Clock()
         rAll = 0
+        collision_n = 0
         while step < max_step:
+            # if step < 100:
+            #     pygame.image.save(screen, "./screen_shot/ep{0:0=3}_step{1:0=3}.png".format(ep,step))
             wheel_id = np.where(map.map>=3)
             wheel = map.map[wheel_id[0], wheel_id[1]]
             s_row, s_col = wheel_id
@@ -92,9 +103,9 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                 sys.exit()
 
             step += 1
-            sys.stdout.write("\rstep: {}/{} | episode: {}/{} | sum reward: {}".format(step,max_step,ep,episode, rAll))
-            sys.stdout.flush()
-            if  np.random.uniform() < greed_e:
+            sys.stdout.write("\rstep: {}/{} | episode: {}/{} | collision: {} | reward: {:.3f} | sum reward: {:.3f}".format(
+                    step, max_step, ep, episode, collision_n, r, rAll))
+            if np.random.uniform() < greed_e:
                 act = np.random.randint(0, 4)
             else:
                 act = np.argmax(Q[s_row,s_col,s_buc,:] + np.random.randn(1, 4)*(1.0/(step + 1)))
@@ -130,12 +141,14 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         if map.map[wheel_id[0], wheel_id[1]-1] == 0:
                             # print("積み下ろす必要がある")
                             r = r3
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0], wheel_id[1]] = 1
                             # 積み下ろし
                             if map.map[wheel_id[0], wheel_id[1]-1] == 2:
                                 map.map[wheel_id[0], wheel_id[1]-1] = 3
-                                r = r2
+                                x_c, y_c = np.where(map.map==0)
+                                r = r2*eval_action(x_c, y_c)
                                 s_buc_next = 0
                             # 非目的地
                             else:
@@ -148,13 +161,15 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         # 積み上げ
                         if map.map[wheel_id[0], wheel_id[1]-1] == 0:
                             map.map[wheel_id[0], wheel_id[1]-1] = 4
-                            r = r1
+                            x_c, y_c = np.where(map.map==0)
+                            r = r1*eval_action(x_c, y_c)
                             s_buc_next = 1
                         # ゴール
                         elif map.map[wheel_id[0], wheel_id[1]-1] == 2:
                             map.map[wheel_id[0], wheel_id[1]-1] = 3
-                            r = r3
+                            r = r5
                             s_buc_next = 0
+                            collision_n += 1
                         # 無し
                         else:
                             map.map[wheel_id[0], wheel_id[1]-1] = 3
@@ -162,7 +177,8 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                             r = def_r
                 else:
                     # print("over the workspace")
-                    r = r3
+                    r = r4
+                    collision_n += 1
 
             if 1 == act:
                 if wheel_id[1] < (map.col-1):
@@ -170,11 +186,13 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         if map.map[wheel_id[0], wheel_id[1]+1] == 0:
                             # print("積み下ろす必要がある")
                             r = r3
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0], wheel_id[1]] = 1
                             if map.map[wheel_id[0], wheel_id[1]+1] == 2:
                                 map.map[wheel_id[0], wheel_id[1]+1] = 3
-                                r = r2
+                                x_c, y_c = np.where(map.map==0)
+                                r = r2*eval_action(x_c, y_c)
                                 s_buc_next = 0
                             else:
                                 map.map[wheel_id[0], wheel_id[1]+1] = 4
@@ -184,19 +202,22 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         map.map[wheel_id[0], wheel_id[1]] = 1
                         if map.map[wheel_id[0], wheel_id[1]+1] == 0:
                             map.map[wheel_id[0], wheel_id[1]+1] = 4
-                            r = r1
+                            x_c, y_c = np.where(map.map==0)
+                            r = r1*eval_action(x_c, y_c)
                             s_buc_next = 1
                         elif map.map[wheel_id[0], wheel_id[1]+1] == 2:
                             map.map[wheel_id[0], wheel_id[1]+1] = 3
-                            r = r3
+                            r = r5
                             s_buc_next = 0
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0], wheel_id[1]+1] = 3
                             s_buc_next = 0
                             r = def_r
                 else:
                     # print("over the workspace")
-                    r = r3
+                    r = r4
+                    collision_n += 1
 
             if 2 == act:
                 if wheel_id[0] > 0:
@@ -204,11 +225,13 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         if map.map[wheel_id[0]-1, wheel_id[1]] == 0:
                             # print("積み下ろす必要がある")
                             r = r3
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0], wheel_id[1]] = 1
                             if map.map[wheel_id[0]-1, wheel_id[1]] == 2:
                                 map.map[wheel_id[0]-1, wheel_id[1]] = 3
-                                r = r2
+                                x_c, y_c = np.where(map.map==0)
+                                r = r2*eval_action(x_c, y_c)
                                 s_buc_next = 0
                             else:
                                 map.map[wheel_id[0]-1, wheel_id[1]] = 4
@@ -218,19 +241,22 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         map.map[wheel_id[0], wheel_id[1]] = 1
                         if map.map[wheel_id[0]-1, wheel_id[1]] == 0:
                             map.map[wheel_id[0]-1, wheel_id[1]] = 4
-                            r = r1
+                            x_c, y_c = np.where(map.map==0)
+                            r = r1*eval_action(x_c, y_c)
                             s_buc_next = 1
                         elif map.map[wheel_id[0]-1, wheel_id[1]] == 2:
                             map.map[wheel_id[0]-1, wheel_id[1]] = 3
-                            r = r3
+                            r = r5
                             s_buc_next = 0
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0]-1, wheel_id[1]] = 3
                             s_buc_next = 0
                             r = def_r
                 else:
                     # print("over the workspace")
-                    r = r3
+                    r = r4
+                    collision_n += 1
 
             if 3 == act:
                 if wheel_id[0] < (map.row-1):
@@ -238,11 +264,13 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         if map.map[wheel_id[0]+1, wheel_id[1]] == 0:
                             # print("積み下ろす必要がある")
                             r = r3
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0], wheel_id[1]] = 1
                             if map.map[wheel_id[0]+1, wheel_id[1]] == 2:
                                 map.map[wheel_id[0]+1, wheel_id[1]] = 3
-                                r = r2
+                                x_c, y_c = np.where(map.map==0)
+                                r = r2*eval_action(x_c, y_c)
                                 s_buc_next = 0
                             else:
                                 map.map[wheel_id[0]+1, wheel_id[1]] = 4
@@ -252,19 +280,22 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
                         map.map[wheel_id[0], wheel_id[1]] = 1
                         if map.map[wheel_id[0]+1, wheel_id[1]] == 0:
                             map.map[wheel_id[0]+1, wheel_id[1]] = 4
-                            r += r1
+                            x_c, y_c = np.where(map.map==0)
+                            r += r1*eval_action(x_c, y_c)
                             s_buc_next = 1
                         elif map.map[wheel_id[0]+1, wheel_id[1]] == 2:
                             map.map[wheel_id[0]+1, wheel_id[1]] = 3
-                            r = r3
+                            r = r5
                             s_buc_next = 0
+                            collision_n += 1
                         else:
                             map.map[wheel_id[0]+1, wheel_id[1]] = 3
                             s_buc_next = 0
                             r = def_r
                 else:
                     # print("over the workspace")
-                    r = r3
+                    r = r4
+                    collision_n += 1
 
             s_row_next, s_col_next = np.where(map.map>=3)
             Q[s_row, s_col, s_buc, act] = Q[s_row, s_col, s_buc, act] +\
@@ -280,32 +311,21 @@ def main(max_step, episode, map_n, greed_e, r1, r2, r3, def_r, test, fps):
             pygame.display.update()
             FPSCLOCK.tick(fps)
         if test != True:
-            if ep%(episode//10) == 0:
+            if ep%5 == 0:
                 np.save(save_path + "/Qtable_ep{0:0=5}.npy".format(ep), Q)
         print()
 
 if __name__ == "__main__":
-    main(max_step=10000,
-         episode=1,
-         map_n=2,
+    main(max_step=20000,
+         episode=20,
+         map_n=5,
          greed_e=0.1,
-         r1=1, # 積み上げ報酬
-         r2=2, # 積み下ろし報酬
-         r3=-1, # 壁ぶつかり報酬
+         r1=5, # 積み上げ報酬
+         r2=10, # 積み下ろし報酬
+         r3=-5, # 積み上げ状態での土砂ぶつかり報酬
+         r4=-0.1, # 壁衝突報酬
+         r5=-10, # 空状態でのゴール通過報酬
          def_r=-0.01, # ただ移動しているだけでは減点されていく
          test=True,
-         fps=100) 
-    # main(max_step=10000,
-    #      episode=200,
-    #      map_n=2,
-    #      greed_e=0.1,
-    #      r1=0.5, # 積み上げ報酬
-    #      r2=2, # 積み下ろし報酬
-    #      r3=-1) # 壁ぶつかり報酬
-    # main(max_step=10000,
-    #      episode=200,
-    #      map_n=3,
-    #      greed_e=0.1,
-    #      r1=0.5, # 積み上げ報酬
-    #      r2=2, # 積み下ろし報酬
-    #      r3=-1) # 壁ぶつかり報酬
+         fps=2000) 
+
